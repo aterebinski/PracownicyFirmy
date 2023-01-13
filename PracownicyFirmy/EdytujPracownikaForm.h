38255732ec572f210eb7deb1f8ae0ea7df047672
Krzysztof Terebiński <k.terebinski@outlook.com>
2023-01-14 00:39:10 +0100

    Gotowa formatka, trzeba dodac jeszcze przejscia po uzyciu przycisku i opcje wyboru w combo boxach.

--- PracownicyFirmy/EdytujPracownikaForm.h
+++ PracownicyFirmy/EdytujPracownikaForm.h
@@ -35,10 +35,27 @@ namespace PracownicyFirmy {
 			}
 		}
 	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ NameLbl;
+	private: System::Windows::Forms::Button^ OkBtn;
+	private: System::Windows::Forms::ComboBox^ StanowiskoCBox;
+
 	protected:
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::ComboBox^ comboBox1;
+
+
+
+	private: System::Windows::Forms::Label^ NazwiskoLbl;
+	private: System::Windows::Forms::Label^ PensjaLbl;
+	private: System::Windows::Forms::Label^ StanowiskoLbl;
+	private: System::Windows::Forms::Label^ LokalizacjaLbl;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::ComboBox^ LokalizacjaCBox;
+
+	private: System::Windows::Forms::Button^ AnulujBtn;
+
+
+
+
 
 	private:
 		/// <summary>
@@ -54,9 +71,17 @@ namespace PracownicyFirmy {
 		void InitializeComponent(void)
 		{
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->NameLbl = (gcnew System::Windows::Forms::Label());
+			this->OkBtn = (gcnew System::Windows::Forms::Button());
+			this->StanowiskoCBox = (gcnew System::Windows::Forms::ComboBox());
+			this->NazwiskoLbl = (gcnew System::Windows::Forms::Label());
+			this->PensjaLbl = (gcnew System::Windows::Forms::Label());
+			this->StanowiskoLbl = (gcnew System::Windows::Forms::Label());
+			this->LokalizacjaLbl = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->LokalizacjaCBox = (gcnew System::Windows::Forms::ComboBox());
+			this->AnulujBtn = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// textBox1
@@ -66,40 +91,130 @@ namespace PracownicyFirmy {
 			this->textBox1->Size = System::Drawing::Size(278, 26);
 			this->textBox1->TabIndex = 0;
 			// 
-			// label1
+			// NameLbl
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(77, 120);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(51, 20);
-			this->label1->TabIndex = 1;
-			this->label1->Text = L"label1";
+			this->NameLbl->AutoSize = true;
+			this->NameLbl->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->NameLbl->Location = System::Drawing::Point(77, 115);
+			this->NameLbl->Name = L"NameLbl";
+			this->NameLbl->Size = System::Drawing::Size(53, 25);
+			this->NameLbl->TabIndex = 1;
+			this->NameLbl->Text = L" Imiê";
 			// 
-			// button1
+			// OkBtn
 			// 
-			this->button1->Location = System::Drawing::Point(206, 475);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(86, 39);
-			this->button1->TabIndex = 2;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
+			this->OkBtn->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->OkBtn->Location = System::Drawing::Point(206, 559);
+			this->OkBtn->Name = L"OkBtn";
+			this->OkBtn->Size = System::Drawing::Size(125, 40);
+			this->OkBtn->TabIndex = 2;
+			this->OkBtn->Text = L"Ok";
+			this->OkBtn->UseVisualStyleBackColor = true;
+			this->OkBtn->Click += gcnew System::EventHandler(this, &EdytujPracownikaForm::OkBtn_Click);
 			// 
-			// comboBox1
+			// StanowiskoCBox
 			// 
-			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(206, 333);
-			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(278, 28);
-			this->comboBox1->TabIndex = 3;
+			this->StanowiskoCBox->FormattingEnabled = true;
+			this->StanowiskoCBox->Location = System::Drawing::Point(206, 355);
+			this->StanowiskoCBox->Name = L"StanowiskoCBox";
+			this->StanowiskoCBox->Size = System::Drawing::Size(278, 28);
+			this->StanowiskoCBox->TabIndex = 3;
+			// 
+			// NazwiskoLbl
+			// 
+			this->NazwiskoLbl->AutoSize = true;
+			this->NazwiskoLbl->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->NazwiskoLbl->Location = System::Drawing::Point(77, 195);
+			this->NazwiskoLbl->Name = L"NazwiskoLbl";
+			this->NazwiskoLbl->Size = System::Drawing::Size(96, 25);
+			this->NazwiskoLbl->TabIndex = 4;
+			this->NazwiskoLbl->Text = L"Nazwisko";
+			// 
+			// PensjaLbl
+			// 
+			this->PensjaLbl->AutoSize = true;
+			this->PensjaLbl->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->PensjaLbl->Location = System::Drawing::Point(77, 275);
+			this->PensjaLbl->Name = L"PensjaLbl";
+			this->PensjaLbl->Size = System::Drawing::Size(72, 25);
+			this->PensjaLbl->TabIndex = 5;
+			this->PensjaLbl->Text = L"Pensja";
+			// 
+			// StanowiskoLbl
+			// 
+			this->StanowiskoLbl->AutoSize = true;
+			this->StanowiskoLbl->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->StanowiskoLbl->Location = System::Drawing::Point(77, 355);
+			this->StanowiskoLbl->Name = L"StanowiskoLbl";
+			this->StanowiskoLbl->Size = System::Drawing::Size(113, 25);
+			this->StanowiskoLbl->TabIndex = 6;
+			this->StanowiskoLbl->Text = L"Stanowisko";
+			// 
+			// LokalizacjaLbl
+			// 
+			this->LokalizacjaLbl->AutoSize = true;
+			this->LokalizacjaLbl->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->LokalizacjaLbl->Location = System::Drawing::Point(77, 435);
+			this->LokalizacjaLbl->Name = L"LokalizacjaLbl";
+			this->LokalizacjaLbl->Size = System::Drawing::Size(109, 25);
+			this->LokalizacjaLbl->TabIndex = 7;
+			this->LokalizacjaLbl->Text = L"Lokalizacja";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(206, 195);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(278, 26);
+			this->textBox2->TabIndex = 8;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(206, 275);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(278, 26);
+			this->textBox3->TabIndex = 9;
+			// 
+			// LokalizacjaCBox
+			// 
+			this->LokalizacjaCBox->FormattingEnabled = true;
+			this->LokalizacjaCBox->Location = System::Drawing::Point(206, 435);
+			this->LokalizacjaCBox->Name = L"LokalizacjaCBox";
+			this->LokalizacjaCBox->Size = System::Drawing::Size(278, 28);
+			this->LokalizacjaCBox->TabIndex = 10;
+			// 
+			// AnulujBtn
+			// 
+			this->AnulujBtn->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->AnulujBtn->Location = System::Drawing::Point(359, 559);
+			this->AnulujBtn->Name = L"AnulujBtn";
+			this->AnulujBtn->Size = System::Drawing::Size(125, 40);
+			this->AnulujBtn->TabIndex = 11;
+			this->AnulujBtn->Text = L"Anuluj";
+			this->AnulujBtn->UseVisualStyleBackColor = true;
 			// 
 			// EdytujPracownikaForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(685, 660);
-			this->Controls->Add(this->comboBox1);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->label1);
+			this->Controls->Add(this->AnulujBtn);
+			this->Controls->Add(this->LokalizacjaCBox);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->LokalizacjaLbl);
+			this->Controls->Add(this->StanowiskoLbl);
+			this->Controls->Add(this->PensjaLbl);
+			this->Controls->Add(this->NazwiskoLbl);
+			this->Controls->Add(this->StanowiskoCBox);
+			this->Controls->Add(this->OkBtn);
+			this->Controls->Add(this->NameLbl);
 			this->Controls->Add(this->textBox1);
 			this->Name = L"EdytujPracownikaForm";
 			this->Text = L"EdytujPracownikaForm";
@@ -108,5 +223,7 @@ namespace PracownicyFirmy {
 
 		}
 #pragma endregion
-	};
+	private: System::Void OkBtn_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
